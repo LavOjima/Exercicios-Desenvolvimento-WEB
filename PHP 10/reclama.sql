b2b1abb0b0b957ce29eab17d56f5cee7c4ca7ed4
Lavínia Ojima <lavinia.ojima@aluno.ifsp.edu.br>
2025-11-03 19:52:01 -0300

    Update reclama.sql

--- PHP 10/reclama.sql
+++ PHP 10/reclama.sql
@@ -6,6 +6,8 @@
 -- Tempo de geração: 30-Out-2025 às 01:41
 -- Versão do servidor: 10.4.32-MariaDB
 -- versão do PHP: 8.2.12
+create database reclama
+use database reclama;
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
